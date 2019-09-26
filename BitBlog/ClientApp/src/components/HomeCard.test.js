2b5bbcf8a3dbc7d93291e009038d051cd85da30a
palm <tauman.exe@gmail.com>
2019-09-26 11:15:47 +0200

    Initialized test suite with Enzyme.

---
+++ BitBlog/ClientApp/src/components/HomeCard.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import HomeCard from "./HomeCard";
+import Home from "./Home";
+
+configure({ adapter: new Adapter() });
+
+describe("<HomeCard />", () => {
+  it("should have an DOM object with the card-image class.", () => {
+    const wrapper = shallow(<HomeCard />);
+    expect(wrapper.exists(".card-image")).toEqual(true);
+  });
+});
